dd160102e6046605e3b01b87a38a8cd908d806d5
kylmps <kyleandrei.mapue.iics@ust.edu.ph>
2021-08-03 12:11:18 +0800

    Initial commit

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+<section>
+  <base-card>
+  <h2>Page not foound</h2>
+  <p>This page could not be found. Go <router-link to="/coaches">here</router-link></p>
+  </base-card>
+  </section>
+</template>
